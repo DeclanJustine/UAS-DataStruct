1f017f5eb7f6acadb9d98742ccc27774a0560165
DeclanJustine <justinedeclans@gmail.com>
2024-05-25 16:15:40 +0700

    first com

---
+++ src/MainProgram.java
@@ -0,0 +1,98 @@
+import strukdat.*;
+class Vertex{
+	String nodeName;
+	Vertex(String name)
+	{
+		this.nodeName = name;
+	}
+
+    @Override
+    public String toString() {
+        return (nodeName);
+    }
+}
+
+
+public class MainProgram {
+    public static void main(String[] args) {
+        Vertex BPN = new Vertex("Bandar Udara Internasional Sultan Aji Muhammad Sulaiman Sepinggan(BPN) - Balikpapan");
+        Vertex BEJ = new Vertex("Bandar Udara Kalimarau(BEJ) - Berau");
+        Vertex AAP = new Vertex("Bandar Udara Aji Pangeran Tumenggung Pranoto(AAP) - Samarinda");
+        Vertex TRK = new Vertex("Bandara Internasional Juwata Tarakan(TRK) - Tarakan");
+        Vertex SMQ = new Vertex("Bandar Udara H. Asan Sampit(SMQ) - Sampit");
+        Vertex PKN = new Vertex("Bandar Udara Iskandar(PKN) - Pangkalanbun");
+        Vertex BWX = new Vertex("Bandar Udara Internasional Banyuwangi(BWX) - Banyuwangi");
+        Vertex LLJ = new Vertex("Bandar Udara Silampari(LLJ) - LubukLinggau");
+        Vertex TJQ = new Vertex("Bandar Udara Internasional H.A.S. Hanandjoeddin(TJQ) - TanjungPandan");
+        Vertex DHX = new Vertex("Bandara Internasional Dhoho Kediri(DHX) - Kediri");
+        Vertex DTB = new Vertex("Bandar Udara Internasional Sisingamangaraja XII(DTB) - Tapanuli");
+        Vertex BDJ = new Vertex("Bandar Udara Internasional Syamsudinnoor(BDJ) - Banjarmasin");
+        Vertex PKY = new Vertex("Bandar Udara Tjilik Riwut(PKY) - Palangkaraya");
+        Vertex PNK = new Vertex("Bandar Udara Internasional Supadio(PNK) - Pontianak");
+        Vertex CGK = new Vertex("Bandar Udara Internasional Soekarno Hatta(CGK) - Jakarta");
+        Vertex MLG = new Vertex("Bandar Udara Abdulrachman Saleh(MLG) - Malang");
+        Vertex SRG = new Vertex("Bandar Udara Jenderal Ahmad Yani(SRG) - Semarang");
+        Vertex SUB = new Vertex("Bandara Internasional Juanda(SUB) - Surabaya");
+        Vertex SOC = new Vertex("Bandar Udara Adi Soemarmo(SOC) - Surakarta");
+        Vertex BDO = new Vertex("Bandar Udara Internasional Husein Sastranegara(BDO) - Bandung");
+        Vertex YIA = new Vertex("Bandara Internasional Yogyakarta(YIA) - Yogyakarta");
+        Vertex BTJ = new Vertex("Bandar Udara Internasional Sultan Iskandar Muda(BTJ) - BandaAceh");
+        Vertex TKG = new Vertex("Bandar Udara Internasional Radin Inten II.(TKG) - BandarLampung");
+        Vertex BKS = new Vertex("Bandar Udara Fatmawati Soekarno(BKS) - Bengkulu");
+        Vertex GNS = new Vertex("Bandar Udara Binaka Gunungsitoli Nias(GNS) - Nias");
+        Vertex DJB = new Vertex("Bandara Internasional Sultan Thaha(DJB) - Jambi");
+        Vertex KNO = new Vertex("Kualanamu International Airport(KNO) - Medan");
+        Vertex PDG = new Vertex("Bandar Udara Internasional Minangkabau(PDG) - Padang");
+        Vertex PLM = new Vertex("Bandar Udara Internasional Sultan Mahmud Badaruddin II(PLM) - Palembang");
+        Vertex PKU = new Vertex("Bandara Internasional Sultan Syarif Kasim II(PKU) - Pekanbaru");
+        Vertex TNJ = new Vertex("Bandara Raja Haji Fisabilillah Tanjung Pinang(TNJ) - TanjungPinang");
+        Vertex BTH = new Vertex("Bandar Udara Internasional Hang Nadim(BTH) - Batam");
+        Vertex PGK = new Vertex("Bandar Udara Depati Amir(PGK) - PangkalPinang");
+
+        WeightedGraph<Vertex> bandara = new WeightedGraph<Vertex>(false); // Undirected
+        // Gak ada ke AAP, SMQ, PKN
+        bandara.addEdge(BPN, BDJ, 342); 
+        bandara.addEdge(BPN, BEJ, 370);
+        bandara.addEdge(BPN, TRK, 512); 
+        bandara.addEdge(BPN, PKY, 344);
+        bandara.addEdge(BPN, PNK, 846);
+
+        bandara.addEdge(BPN, CGK, 1241);
+
+        bandara.addEdge(BPN, SRG, 958);
+        bandara.addEdge(BPN, YIA, 1025);
+        bandara.addEdge(BPN, SOC, 970);
+        bandara.addEdge(BPN, SUB, 809);
+        bandara.addEdge(BPN, BTH, 1451);
+        bandara.addEdge(BPN, KNO, 2091);
+
+        // Gak ada ke TRK, SMQ, PKN, PKY, BDJ, PNK; udah ada BPN
+        bandara.addEdge(BEJ, AAP, 284);
+        bandara.addEdge(BEJ, CGK, 1485);
+
+        // Gak ada ke BPN, TRK, SMQ, PKN, BDJ, PKY, PNK; udah ada BEJ 
+        bandara.addEdge(AAP, CGK, 1309); 
+
+        // Gak ada ke BEJ, AAP, SMQ, PKN, PKY, PNK, BDJ; udah ada BPN
+        bandara.addEdge(TRK, CGK, 1595);
+
+        // Gak ada ke PKY, PKN, BPN, BEJ, AAP, TRK, BDJ, PNK;
+        bandara.addEdge(SMQ, CGK, 785);
+
+        // Gak ada ke BEJ, AAP, TRK, SMQ, BDJ, PKY, PNK; udah ada BPN
+        bandara.addEdge(PKN, CGK, 337);
+
+        // Gak ada ke BEJ, AAP, TRK, SMQ, PKN, PKY, PNK ; udah ada BPN
+        bandara.addEdge(BDJ, CGK, 917);
+
+        // Gak ada BEJ, AAP, TRK, SMQ, PKN, BDJ, PNK; udah ada BPN
+        bandara.addEdge(PKY, CGK, 901);
+
+        // Gak ada BEJ, AAP, TRK, SMQ, PKN, BDJ, PKY ; udah ada BPN
+        bandara.addEdge(PNK, CGK, 739);
+
+        // KALIMANTAN KELAR
+        // -----------------------------------------------------------------------------------------
+
+    }
+}
---
+++ src/strukdat/WeightedGraph.java
@@ -0,0 +1,194 @@
+package strukdat;
+
+import java.util.*;
+
+class Edge<T> { 
+    T neighbor; // connected vertex
+    int weight; // weight
+
+    // Constructor, Time O(1) Space O(1)
+    public Edge(T v, int w) {
+        this.neighbor = v; 
+        this.weight = w;
+    }
+
+    // Time O(1) Space O(1)
+    @Override
+    public String toString() {
+        return "(" + neighbor + "," + weight + ")";
+    }
+
+    public T getNeighbor(){
+		return neighbor;
+	}
+}
+
+public class WeightedGraph<T> { 
+    private Map<T, SingleList<Edge<T>>> adj;
+    boolean directed;
+
+    // Constructor, Time O(1) Space O(1)
+    public WeightedGraph(boolean type) { 
+        adj = new HashMap<>();
+        directed = type; // false: undirected, true: directed
+    }
+
+    // Add edges including adding nodes, Time O(1) Space O(1)
+    public void addEdge(T a, T b, int w) {
+        adj.putIfAbsent(a, new SingleList<>()); // add vertex
+        adj.putIfAbsent(b, new SingleList<>()); // add vertex
+        Edge<T> edge1 = new Edge<>(b, w);
+        adj.get(a).pushQ(edge1); // add edge
+        if (!directed) { // undirected
+            Edge<T> edge2 = new Edge<>(a, w);
+            adj.get(b).pushQ(edge2);
+        }            
+    }
+
+    // Print graph as hashmap, Time O(V+E), Space O(1)
+    public void printGraph() {
+        for (T key: adj.keySet()) {
+            System.out.print(key.toString() + " : ");
+            SingleList<Edge<T>> thelist = adj.get(key);
+            Node<Edge<T>> curr = thelist.head;
+            while(curr != null) {
+                System.out.print(curr.data);
+                curr = curr.next;
+            }
+            System.out.println();
+        }
+    }
+
+    // DFS 
+    public void dfsTraversal(T key) {
+        if (!adj.containsKey(key)) // invalid input
+            return;
+        HashMap<T, Boolean> visited = new HashMap<>();
+        helper(key, visited);
+        System.out.println();
+    }
+
+    // DFS helper
+    private void helper(T v, HashMap<T, Boolean> visited) {
+        visited.put(v, true);
+        System.out.print(v.toString() + " ");
+        
+        SingleList<Edge<T>> edges = adj.get(v);
+        Node<Edge<T>> curr = edges.head;
+    
+        while (curr != null) {
+            Edge<T> edge = curr.data;
+            T neighbor = edge.neighbor;
+            if (visited.get(neighbor) == null) {
+                helper(neighbor, visited);
+            }
+            curr = curr.next;
+        }
+    }
+
+    // BFS
+    public void bfsTraversal(T key) {
+        if (!adj.containsKey(key)) // invalid input
+            return;
+        Queue<T> q = new LinkedList<>(); 
+        HashMap<T, Boolean> visited = new HashMap<>(); 
+        q.add(key); 
+        visited.put(key, true); 
+        while (!q.isEmpty()) { 
+            T v = q.poll(); 
+            System.out.print(v.toString() + " ");        
+    
+            SingleList<Edge<T>> edges = adj.get(v);
+            Node<Edge<T>> curr = edges.head;
+    
+            while (curr != null) {
+                Edge<T> edge = curr.data;
+                T neighbor = edge.neighbor;
+                if (visited.get(neighbor) == null) { 
+                    q.add(neighbor); 
+                    visited.put(neighbor, true); 
+                }
+                curr = curr.next;
+            }
+        } 
+        System.out.println(); 
+    }
+
+    public void deleteEdge(T a, T b) {
+		SingleList<Edge<T>> allEdgeA = adj.get(a);
+
+		int searchIndex = 0;
+		Edge<T> deleteEdgeA = allEdgeA.get(searchIndex);
+		while(deleteEdgeA.getNeighbor() != b && deleteEdgeA != null) {
+			searchIndex++;
+			deleteEdgeA = allEdgeA.get(searchIndex);
+		}
+		if(deleteEdgeA == null) {
+			return;
+		}
+		adj.get(a).remove(deleteEdgeA);
+            
+		if (!directed) { //undirected
+			SingleList<Edge<T>> allEdgeB = adj.get(b);
+
+			searchIndex = 0;
+			Edge<T> deleteEdge = allEdgeB.get(searchIndex);
+			while(deleteEdge.getNeighbor() != a && deleteEdge != null) {
+				searchIndex++;
+				deleteEdge = allEdgeB.get(searchIndex);
+			}
+			if(deleteEdge == null) {
+				return;
+			}
+			
+			adj.get(b).remove(deleteEdge);
+		}
+    }
+    /*
+     * Shortest Paths dari vertex v ke semua vertex lainnya 
+     * menggunakan Algoritma Dijkstra
+     * ==========================================================
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public void dijkstra(T start) { // N = number of nodes
+        Map<T, Integer> res = new HashMap<>(); 
+        PriorityQueue<Map.Entry<T, Integer>> pq = new PriorityQueue<>((a,b) -> (int)(a.getValue() - b.getValue()));           
+        Map<T, T> previous = new HashMap<>();
+
+        for (T key : adj.keySet()) 
+            res.put(key, Integer.MAX_VALUE);
+        pq.offer(new AbstractMap.SimpleEntry(start, 0)); 
+        res.put(start, 0); 
+        while (!pq.isEmpty()) { 
+            T u = pq.poll().getKey(); 
+            SingleList<Edge<T>> thelist = adj.get(u);
+            Node<Edge<T>> curr = thelist.head;
+            while (curr != null) {
+                T v = curr.data.neighbor;
+                int weight = curr.data.weight; 
+                if (res.get(v) > res.get(u) + weight) { 
+                    res.put(v, res.get(u) + weight); 
+                    pq.offer(new AbstractMap.SimpleEntry(v, res.get(v))); 
+                    previous.put(v, u);
+                }
+                curr = curr.next; 
+            }
+        }
+
+        for (T vertex : res.keySet()) {
+            System.out.print("Shortest Path dari " + start + " ke " + vertex + " dengan weight: " + res.get(vertex) + " : ");
+            printPath(vertex, previous);
+            System.out.println();
+        }
+    }
+
+    public void printPath(T target, Map<T, T> previous) {
+        SingleList<T> path = new SingleList<>();
+        T node = target;
+        while (node!= null) {
+            path.pushS(node);
+            node = previous.get(node);
+        }
+        path.cetakList();
+    }
+}
